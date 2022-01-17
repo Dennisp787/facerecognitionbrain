54860b4282e20cbee264dd89f53854df7308d529
Dennis Phillips <dennisphillips@Denniss-MacBook-Pro.local>
2022-01-17 17:30:28 -0500

    fixing particles effect

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ import Register from './components/Register/Register';
 
 const particlesOption = {
       particles: {
-            		"number": {
-                  "value": 100,
+
+                  "number": {
+                    "value": 100
+                  },
                   "density": {
-                    "enable": true,
-                    "value_area": 800
-                  }
+                      "enable": true,
+                      "value_area": 800
+                  },
+                  "move": {
+                    "enable": true
+                },
+                  "links": {
+                    "enable": true
                 }
       }
     };
